142841c479844fa69125b40a3f686836bf182686
John D <john.davenport71@gmail.com>
2019-09-12 21:20:02 -0400

    Merged 404-error branch to save refactoring changes made.

---
+++ public/errors/404-error.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once('../../private/initialize.php');
+include(SHARED_PATH . '/header.php');
+
+?>
+<body>
+    <h1>404 Error</h1>
+
+    <p>The page you are looking wasn't found</p>
+
+</body>
+<?php include(SHARED_PATH . '/footer.php'); ?>
