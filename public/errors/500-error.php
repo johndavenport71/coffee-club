8dd851693462ca48a84a797e950cdea5e3efe3f3
John D <john.davenport71@gmail.com>
2019-09-13 10:23:47 -0400

    Created functions for displaying custom error pages.

---
+++ public/errors/500-error.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once('../../private/initialize.php');
+include(SHARED_PATH . '/header.php');
+
+?>
+<body>
+    <h1>500 Internal Server Error</h1>
+
+    <p>Something went wrong.</p>
+
+</body>
+<?php include(SHARED_PATH . '/footer.php'); ?>
