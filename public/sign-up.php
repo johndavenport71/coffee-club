2d51f681924beb976815ddd50ccec22147bdf26d
John D <john.davenport71@gmail.com>
2019-09-29 17:41:08 -0400

    Added check for existing email addresses

--- public/sign-up.php
+++ public/sign-up.php
@@ -2,19 +2,30 @@
     require_once('../private/initialize.php'); 
     $page_title = 'Member Sign Up - Coffee Club'; 
     include(SHARED_PATH . '/header.php'); 
+
+    $fname = '';
+    $lname = '';
+    $email = '';
+
+    if(isset($_GET['fname'])) {
+        $fname = h($_GET['fname']);
+        $lname = h($_GET['lname']);
+        $email = h($_GET['email']);
+        echo '<p class="error">A user with that email already exists!</p>';
+    }
 ?>
 
 <main class="sign-up">
     <h2>New Member Sign Up</h2>
     <form action="register-member.php" method="POST">
         <label for="fname">First Name:</label>
-        <input type="text" id="fname" name="fname" required><br>
+        <input type="text" id="fname" name="fname" required value="<?= $fname ?>"><br>
         
         <label for="lname">Last Name:</label>
-        <input type="text" id="lname" name="lname" required><br>
+        <input type="text" id="lname" name="lname" required value="<?= $lname ?>"><br>
         
         <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br>
+        <input type="email" id="email" name="email" required value="<?= $email ?>"><br>
 
         <label for="pass">Password:</label>
         <input type="password" id="pass" name="pass" required><br>
